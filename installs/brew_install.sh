2dbc56314510a27d7c324cb052a1c8ac8be5f8b7
Frederic Masion <f.masion@humans-it.com>
2018-11-29 14:39:24 +0100

    brew basics

---
+++ installs/brew_install.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# Check for Homebrew,
+# Install if we don't have it
+if test ! "$(which brew)"; then
+  echo "Installing homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Update homebrew recipes
+brew update
+
+# Install GNU core utilities (those that come with OS X are outdated)
+brew install coreutils
+
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
+brew install findutils
+
+# Install Bash 4
+brew install bash
+
+# Install more recent versions of some OS X tools
+brew tap homebrew/dupes
+brew install homebrew/dupes/grep
+
+binaries=(
+  git
+  git-flow
+  node
+  nvm
+  yarn
+  sbt
+  htop
+  bash
+  bash-completion
+  readline
+  python
+  cmake
+  coreutils
+  findutils
+  gdbm
+  gettext  
+  grep
+  icdiff
+  icu4c
+  libffi
+  libpng
+  libssh2
+  libyaml
+  lua
+  mas
+  openssl
+  openssl@1.1
+  imagemagick
+  awscli
+  wget
+  jq
+  rpm
+)
+
+echo "installing binaries..."
+brew install "${binaries[@]}"
+
+brew cleanup
+
